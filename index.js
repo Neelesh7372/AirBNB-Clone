011bc912809042007bb342716fdd3e957958de9e
Neelesh7372 <neeleshsharma.kgp90@gmail.com>
2023-04-08 22:33:16 +0530

    amplify-init

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
+// index.js
+
+import 'react-native-url-polyfill/auto';
+import 'react-native-get-random-values';
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+import { Amplify } from 'aws-amplify';
+import awsmobile from './src/aws-exports';
+
+Amplify.configure(awsmobile);
 
 AppRegistry.registerComponent(appName, () => App);
